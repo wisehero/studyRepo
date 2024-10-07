9aaf46c47038ec2ca9ee45e58671846e15d16e79
wisehero <godjw96@gmail.com>
2024-10-07 16:48:25 +0900

    로그 추적기 V2(파라미터로 TraceId 요청 별 일치화)

---
+++ springadvanced/src/main/java/wisehero/springadvanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,33 @@
+package wisehero.springadvanced.app.v2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import wisehero.springadvanced.trace.TraceStatus;
+import wisehero.springadvanced.trace.hellotrace.HelloTraceV2;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+	private final OrderServiceV2 orderService;
+	private final HelloTraceV2 trace;
+
+	@GetMapping("/v2/request")
+	public String request(String itemId) {
+		TraceStatus status = null;
+
+		try {
+			status = trace.begin("OrderController.request()");
+
+			orderService.orderItem(status.getTraceId(), itemId);
+
+			trace.end(status);
+			return "ok";
+		} catch (Exception e) {
+			trace.exception(status, e);
+			throw e;
+		}
+	}
+}
---
+++ springadvanced/src/main/java/wisehero/springadvanced/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,43 @@
+package wisehero.springadvanced.app.v2;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import wisehero.springadvanced.trace.TraceId;
+import wisehero.springadvanced.trace.TraceStatus;
+import wisehero.springadvanced.trace.hellotrace.HelloTraceV2;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV2 {
+
+	private final HelloTraceV2 trace;
+
+	public void save(TraceId traceId, String itemId) {
+
+		TraceStatus status = null;
+		try {
+			status = trace.beginSync(traceId, "OrderRepository.save()");
+
+			// 저장 로직
+			if (itemId.equals("ex")) {
+				throw new IllegalStateException("예외 발생");
+			}
+			sleep(1000);
+
+			trace.end(status);
+		} catch (Exception e) {
+			trace.exception(status, e);
+			throw e;
+		}
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
