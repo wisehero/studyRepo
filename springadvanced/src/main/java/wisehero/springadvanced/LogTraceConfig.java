b244b78dae7fc55eff2aef523d530001d0520f9f
wisehero <godjw96@gmail.com>
2024-10-10 09:26:44 +0900

    ThreadLocal 적용

--- springadvanced/src/main/java/wisehero/springadvanced/LogTraceConfig.java
+++ springadvanced/src/main/java/wisehero/springadvanced/LogTraceConfig.java
@@ -3,14 +3,15 @@ package wisehero.springadvanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import wisehero.springadvanced.trace.logtrace.FieldLogTrace;
 import wisehero.springadvanced.trace.logtrace.LogTrace;
+import wisehero.springadvanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
 	@Bean
 	public LogTrace logTrace() {
-		return new FieldLogTrace();
+		// return new FieldLogTrace();
+		return new ThreadLocalLogTrace();
 	}
 }
