d1f8788919bda98c091211dd6efb1fa43e21c3c4
wisehero <godjw96@gmail.com>
2024-10-10 16:10:40 +0900

    인터페이스와 구현 클래스로 스프링 빈 수동 등록

--- springadvanced/src/main/java/wisehero/springadvanced/SpringadvancedApplication.java
+++ springadvanced/src/main/java/wisehero/springadvanced/SpringadvancedApplication.java
@@ -2,8 +2,12 @@ package wisehero.springadvanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+import wisehero.springadvanced.proxy.config.AppV1Config;
+
+@SpringBootApplication(scanBasePackages = "wisehero.springadvanced.proxy")
+@Import(AppV1Config.class)
 public class SpringadvancedApplication {
 
 	public static void main(String[] args) {
