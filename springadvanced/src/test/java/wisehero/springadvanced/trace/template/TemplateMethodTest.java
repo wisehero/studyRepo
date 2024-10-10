8af40ee76cd54a441bf87557be5ae94fee281d10
wisehero <godjw96@gmail.com>
2024-10-10 09:52:07 +0900

    템플릿 메서드 패턴 - 익명 내부 클래스 활용

--- springadvanced/src/test/java/wisehero/springadvanced/trace/template/TemplateMethodTest.java
+++ springadvanced/src/test/java/wisehero/springadvanced/trace/template/TemplateMethodTest.java
@@ -44,4 +44,26 @@ public class TemplateMethodTest {
 		AbstractTemplate template2 = new SubClassLogic2();
 		template2.execute();
 	}
+
+	// 익명 내부 클래스를 활용한 템플릿 메서드 패턴
+	@Test
+	void templateMethodV2() {
+		AbstractTemplate template1 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		};
+		log.info("클래스 이름1={}", template1.getClass());
+		template1.execute();
+
+		AbstractTemplate template2 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직2 실행");
+			}
+		};
+		log.info("클래스 이름2={}", template2.getClass());
+		template2.execute();
+	}
 }
