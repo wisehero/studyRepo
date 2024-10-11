884f07e9d45bc1ce6cd91130e88ef7274325ea7f
wisehero <godjw96@gmail.com>
2024-10-11 10:59:49 +0900

    리플렉션 - 리플렉션 예제 코드

--- springadvanced/src/test/java/wisehero/springadvanced/proxy/jdkdynamic/ReflectionTest.java
+++ springadvanced/src/test/java/wisehero/springadvanced/proxy/jdkdynamic/ReflectionTest.java
@@ -1,5 +1,8 @@
 package wisehero.springadvanced.proxy.jdkdynamic;
 
+import java.lang.reflect.Method;
+
+import org.assertj.core.api.IterableAssert;
 import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +29,43 @@ public class ReflectionTest {
 		// 위의 로직은 메서드 호출부만 제외한 나머지가 모두 중복이다.
 	}
 
+	@Test
+	void reflection1() throws Exception {
+		// 클래스 정보
+		Class classHello = Class.forName("wisehero.springadvanced.proxy.jdkdynamic.ReflectionTest$Hello");
+
+		Hello target = new Hello();
+		// callA 메서드 정보
+		Method methodCallA = classHello.getMethod("callA");
+		// invoke는 획득한 메서드 메타정보로 실제 인스턴스의 메서드를 호출
+		Object result1 = methodCallA.invoke(target);
+		log.info("result1={}", result1);
+
+		// callB 메서드 정보
+		Method methodCallB = classHello.getMethod("callB");
+		Object result2 = methodCallB.invoke(target);
+		log.info("result2={}", result2);
+	}
+
+	@Test
+	void reflection2() throws Exception {
+		Class classHello = Class.forName("wisehero.springadvanced.proxy.jdkdynamic.ReflectionTest$Hello");
+
+		Hello target = new Hello();
+		Method methodCallA = classHello.getMethod("callA"); // 여기서 런타임 에러 발생 가능성 callA가 아니라 callZ라면?
+		dynamicCall(methodCallA, target);
+
+		Method methodCallB = classHello.getMethod("callB");
+		dynamicCall(methodCallB, target);
+	}
+
+	private void dynamicCall(Method method, Object object) throws Exception {
+		log.info("start");
+		Object result = method.invoke(object);
+		log.info("result={}", result);
+
+	}
+
 	@Slf4j
 	static class Hello {
 		public String callA() {
@@ -38,4 +78,5 @@ public class ReflectionTest {
 			return "B";
 		}
 	}
+
 }
