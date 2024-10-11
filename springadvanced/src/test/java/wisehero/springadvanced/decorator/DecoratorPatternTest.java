9942af8a56b28b1faa9d10259f04a4a46137739d
wisehero <godjw96@gmail.com>
2024-10-11 10:02:57 +0900

    데코레이터 패턴 - 시간 데코레이터 추가

--- springadvanced/src/test/java/wisehero/springadvanced/decorator/DecoratorPatternTest.java
+++ springadvanced/src/test/java/wisehero/springadvanced/decorator/DecoratorPatternTest.java
@@ -7,6 +7,7 @@ import wisehero.springadvanced.decorator.code.Component;
 import wisehero.springadvanced.decorator.code.DecoratorPatternClient;
 import wisehero.springadvanced.decorator.code.MessageDecorator;
 import wisehero.springadvanced.decorator.code.RealComponent;
+import wisehero.springadvanced.decorator.code.TimeDecorator;
 
 @Slf4j
 public class DecoratorPatternTest {
@@ -24,6 +25,16 @@ public class DecoratorPatternTest {
 		Component realComponent = new RealComponent();
 		Component messageDecorator = new MessageDecorator(realComponent);
 		DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+
+		client.execute();
+	}
+
+	@Test
+	void decorator2() {
+		Component realComponent = new RealComponent();
+		Component messageDecorator = new MessageDecorator(realComponent);
+		Component timeDecorator = new TimeDecorator(messageDecorator);
+		DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
 
 		client.execute();
 	}
