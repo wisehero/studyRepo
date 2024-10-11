7cf094419bb24b3b0d90198938c2d07e626f9508
wisehero <godjw96@gmail.com>
2024-10-11 09:54:52 +0900

    프록시 패턴 예제 코드 - 캐싱

--- springadvanced/src/test/java/wisehero/springadvanced/proxy/ProxyPatternTest.java
+++ springadvanced/src/test/java/wisehero/springadvanced/proxy/ProxyPatternTest.java
@@ -2,7 +2,9 @@ package wisehero.springadvanced.proxy;
 
 import org.junit.jupiter.api.Test;
 
+import wisehero.springadvanced.proxy.code.CacheProxy;
 import wisehero.springadvanced.proxy.code.ProxyPatternClient;
+import wisehero.springadvanced.proxy.code.Subject;
 
 public class ProxyPatternTest {
 
@@ -10,6 +12,17 @@ public class ProxyPatternTest {
 	void noProxyTest() {
 		RealSubject realSubject = new RealSubject(); // 실제 객체를 생성해서 사용하므로 프록시가 아니다.
 		ProxyPatternClient client = new ProxyPatternClient(realSubject);
+		client.execute();
+		client.execute();
+		client.execute();
+	}
+
+	@Test
+	void cacheProxyTest() {
+		Subject realSubject = new RealSubject();
+		Subject cacheProxy = new CacheProxy(realSubject);
+
+		ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
 		client.execute();
 		client.execute();
 		client.execute();
