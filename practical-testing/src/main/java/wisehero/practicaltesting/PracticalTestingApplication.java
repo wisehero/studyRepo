8dec610e12f36aa53ae2081520a9910d2c8d43b3
wisehero <godjw96@gmail.com>
2024-10-15 20:54:30 +0900

    [인프런] - Practical Testing init

---
+++ practical-testing/src/main/java/wisehero/practicaltesting/PracticalTestingApplication.java
@@ -0,0 +1,13 @@
+package wisehero.practicaltesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalTestingApplication.class, args);
+	}
+
+}
