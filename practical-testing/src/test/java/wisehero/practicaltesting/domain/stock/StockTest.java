31e758a13dea41a7e43d996c431ce851a17c27ad
wisehero <godjw96@gmail.com>
2024-10-23 20:00:15 +0900

    @DynamicTest

--- practical-testing/src/test/java/wisehero/practicaltesting/domain/stock/StockTest.java
+++ practical-testing/src/test/java/wisehero/practicaltesting/domain/stock/StockTest.java
@@ -2,8 +2,13 @@ package wisehero.practicaltesting.domain.stock;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 class StockTest {
 
@@ -46,5 +51,34 @@ class StockTest {
 		assertThatThrownBy(() -> stock.decreaseQuantity(quantity))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("차감할 수량이 없습니다.");
+	}
+
+	@DisplayName("재고 차감 시나리오")
+	@TestFactory
+	Collection<DynamicTest> stockDeductionDynamicTest() {
+		// given
+		Stock stock = Stock.create("001", 1);
+
+		return List.of(
+			DynamicTest.dynamicTest("재고를 주어진 개수만큼 차감할 수 있다.", () -> {
+				// given
+				int quantity = 1;
+
+				// when
+				stock.decreaseQuantity(quantity);
+
+				// then
+				assertThat(stock.getQuantity()).isZero();
+			}),
+			DynamicTest.dynamicTest("재고보다 많은 수의 수량으로 차감 시도하는 경우 예외가 발생한다.", () -> {
+				// given
+				int quantity = 1;
+
+				// expected
+				assertThatThrownBy(() -> stock.decreaseQuantity(quantity))
+					.isInstanceOf(IllegalArgumentException.class)
+					.hasMessage("차감할 수량이 없습니다.");
+			})
+		);
 	}
 }
