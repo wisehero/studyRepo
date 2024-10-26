3761f5df64ca726523c4e9b29fa13c3f0f2891f3
wisehero <godjw96@gmail.com>
2024-10-26 22:40:38 +0900

    포인트컷 만들어보기

--- springadvanced/src/test/java/wisehero/springadvanced/proxy/advisor/AdvisorTest.java
+++ springadvanced/src/test/java/wisehero/springadvanced/proxy/advisor/AdvisorTest.java
@@ -1,6 +1,11 @@
 package wisehero.springadvanced.proxy.advisor;
 
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
@@ -27,5 +32,55 @@ public class AdvisorTest {
 
 		proxy.save();
 		proxy.find();
+	}
+
+	@Test
+	@DisplayName("직접 만든 포인트컷")
+	void advisorTest2() {
+		ServiceImpl target = new ServiceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+		proxyFactory.addAdvisor(advisor);
+		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+
+		proxy.save();
+		proxy.find();
+	}
+
+	static class MyPointcut implements Pointcut {
+
+		@Override
+		public ClassFilter getClassFilter() {
+			return ClassFilter.TRUE;
+		}
+
+		@Override
+		public MethodMatcher getMethodMatcher() {
+			return new MyMethodMatcher();
+		}
+	}
+
+	static class MyMethodMatcher implements MethodMatcher {
+		private String matchName = "save";
+
+		@Override
+		public boolean matches(Method method, Class<?> targetClass) {
+			// save라는 이름이 넘어오면 true를 반환하도록 설정
+			boolean result = method.getName().equals(matchName);
+			log.info("포인트컷 호출 method={} targetClass={}", method.getName(), targetClass);
+			log.info("포인트컷 결과 result={}", result);
+			return result;
+		}
+
+		// 아래의 메서드가 true이면 아래의 matches가 사용된다.
+		@Override
+		public boolean isRuntime() {
+			return false;
+		}
+
+		@Override
+		public boolean matches(Method method, Class<?> targetClass, Object... args) {
+			throw new UnsupportedOperationException();
+		}
 	}
 }
