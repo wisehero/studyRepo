df54b31c7bd301db99d12f0575109d86154d4f86
wisehero <godjw96@gmail.com>
2024-10-27 20:46:52 +0900

    ByteArrayStream, PrintStream

---
+++ java-adv2/src/io/start/PrintStreamMain.java
@@ -0,0 +1,15 @@
+package io.start;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+public class PrintStreamMain {
+	public static void main(String[] args) throws IOException {
+		PrintStream printStream = System.out;
+
+		byte[] bytes = "Hello, World!\n".getBytes(StandardCharsets.UTF_8);
+		printStream.write(bytes);
+		printStream.println("print!");
+	}
+}
