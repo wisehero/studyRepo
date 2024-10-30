5348ca19bd14dc3626affb017ccc64006498526b
wisehero <godjw96@gmail.com>
2024-10-30 11:34:27 +0900

    기본 설정 클래스

---
+++ spring-aop/src/test/java/wisehero/springaop/AopTest.java
@@ -0,0 +1,40 @@
+package wisehero.springaop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+import wisehero.springaop.order.OrderRepository;
+import wisehero.springaop.order.OrderService;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+	@Autowired
+	OrderService orderService;
+
+	@Autowired
+	OrderRepository orderRepository;
+
+	@Test
+	void aopInfo() {
+		log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+		log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+	}
+
+	@Test
+	void success() {
+		orderService.orderItem("itemA");
+	}
+
+	@Test
+	void exception() {
+		Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("예외 발생!");
+	}
+}
