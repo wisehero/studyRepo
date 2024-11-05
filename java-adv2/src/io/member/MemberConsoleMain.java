d5ae83210b900d727ff01a1bb70634d3bb152732
wisehero <godjw96@gmail.com>
2024-11-05 23:08:57 +0900

    회원 관리 예제 - ObjectStream 저장(직렬화 객체 저장하기)

--- java-adv2/src/io/member/MemberConsoleMain.java
+++ java-adv2/src/io/member/MemberConsoleMain.java
@@ -3,13 +3,14 @@ package io.member;
 import java.util.List;
 import java.util.Scanner;
 
-import io.member.impl.DataMemberRepository;
+import io.member.impl.ObjectMemberRepository;
 
 public class MemberConsoleMain {
 
 	// private static final MemberRepository repository = new MemoryMemberRepository();
 	// private static final MemberRepository repository = new FileMemberRepository();
-	private static final MemberRepository repository = new DataMemberRepository();
+	// private static final MemberRepository repository = new DataMemberRepository();
+	private static final MemberRepository repository = new ObjectMemberRepository();
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
---
+++ java-adv2/src/io/member/impl/ObjectMemberRepository.java
@@ -0,0 +1,42 @@
+package io.member.impl;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.member.Member;
+import io.member.MemberRepository;
+
+public class ObjectMemberRepository implements MemberRepository {
+
+	private static final String FILE_PATH = "temp/member-obj.dat";
+
+	@Override
+	public void add(Member member) {
+		List<Member> members = findAll();
+		members.add(member);
+
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
+			oos.writeObject(members);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public List<Member> findAll() {
+		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
+			Object findObject = ois.readObject();
+			return (List<Member>)findObject;
+		} catch (FileNotFoundException e) {
+			return new ArrayList<>();
+		} catch (IOException | ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
