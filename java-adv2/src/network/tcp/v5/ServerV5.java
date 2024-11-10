a44ba036dd223ca5d21bc77b3213c2f137968ca7
wisehero <godjw96@gmail.com>
2024-11-10 14:37:02 +0900

    네트워크 프로그래밍 - 자원 정리 적용 V2 (try-with-resources)

---
+++ java-adv2/src/network/tcp/v5/ServerV5.java
@@ -0,0 +1,29 @@
+package network.tcp.v5;
+
+import static util.MyLogger.*;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import network.tcp.v4.SessionV4;
+
+public class ServerV5 {
+
+	private static final int PORT = 12345;
+
+	public static void main(String[] args) throws IOException {
+		log("서버 시작");
+		ServerSocket serverSocket = new ServerSocket(PORT);
+		log("서버 소켓 시작 - 리스닝 포트: " + PORT);
+
+		while (true) {
+			Socket socket = serverSocket.accept();
+			log("소켓 연결: " + socket);
+
+			SessionV5 session = new SessionV5(socket);
+			Thread thread = new Thread(session);
+			thread.start();
+		}
+	}
+}
