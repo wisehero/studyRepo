cd9c00e02e8a164b78daa13ef7cce4245bbe6be2
wisehero <godjw96@gmail.com>
2024-11-17 00:36:47 +0900

    네트워크 프로그래밍 - HTTP Server V2(스레드 여러개)

---
+++ java-adv2/src/was/v2/HttpServerV2.java
@@ -0,0 +1,29 @@
+package was.v2;
+
+import static util.MyLogger.*;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HttpServerV2 {
+
+	private final ExecutorService es = Executors.newFixedThreadPool(10); // 동시에 10개 스레드 사용
+	private final int port;
+
+	public HttpServerV2(int port) {
+		this.port = port;
+	}
+
+	public void start() throws IOException {
+		ServerSocket serverSocket = new ServerSocket(port);
+		log("서버 시작 port: " + port);
+
+		while (true) {
+			Socket socket = serverSocket.accept();
+			es.submit(new HttpRequestHandlerV2(socket));
+		}
+	}
+}
