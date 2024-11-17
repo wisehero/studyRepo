b6ac61fa121d8aef51211c878aef5cf3e54f69af
wisehero <godjw96@gmail.com>
2024-11-18 04:08:46 +0900

    리플렉션 - 생성자 활용

---
+++ java-adv2/src/reflection/ConstructV1.java
@@ -0,0 +1,21 @@
+package reflection;
+
+import java.lang.reflect.Constructor;
+
+public class ConstructV1 {
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class<?> aClass = Class.forName("reflection.data.BasicData");
+
+		System.out.println("============== constructors() ==============");
+		Constructor<?>[] constructors = aClass.getConstructors();
+		for (Constructor<?> constructor : constructors) {
+			System.out.println(constructor);
+		}
+
+		System.out.println("============== declaredConstructors() ==============");
+		Constructor<?>[] declaredConstructors = aClass.getDeclaredConstructors();
+		for (Constructor<?> declaredConstructor : declaredConstructors) {
+			System.out.println(declaredConstructor);
+		}
+	}
+}
