d05178b1efb39ea680070cc36e5e5c865849fe14
wisehero <godjw96@gmail.com>
2024-11-21 05:25:53 +0900

    애노테이션 - 애노테이션으로 검증 로직 개선

--- java-adv2/src/annotation/validator/Team.java
+++ java-adv2/src/annotation/validator/Team.java
@@ -2,7 +2,9 @@ package annotation.validator;
 
 public class Team {
 
+	@NotEmpty(message = "이름이 비어있습니다.")
 	private String name;
+	@Range(min = 1, max = 100, message = "팀 인원은 1과 999 사이여야 합니다.")
 	private int memberCount;
 
 	public Team(String name, int memberCount) {
--- java-adv2/src/annotation/validator/User.java
+++ java-adv2/src/annotation/validator/User.java
@@ -2,7 +2,9 @@ package annotation.validator;
 
 public class User {
 
+	@NotEmpty(message = "이름이 비어있습니다.")
 	private String name;
+	@Range(min = 1, max = 100, message = "나이는 1과 100 사이여야 합니다.")
 	private int age;
 
 	public User(String name, int age) {
