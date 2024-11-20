bc5e3ef7fe7a2bd72b19c6302f5c077895f36a81
wisehero <godjw96@gmail.com>
2024-11-21 05:44:16 +0900

    애노테이션 서블릿 - V1

---
+++ java-adv2/src/was/httpserver/annotation/Mapping.java
@@ -0,0 +1,14 @@
+package was.httpserver.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface Mapping {
+	String value();
+}
