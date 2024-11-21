62e6cb0e54eafa9488e230738a4d5e9e12bf147a
wisehero <godjw96@gmail.com>
2024-11-22 05:05:39 +0900

    애노테이션 서블릿 - V3(최적화)

--- java-adv2/src/was/v8/ServerMainV8.java
+++ java-adv2/src/was/v8/ServerMainV8.java
@@ -8,6 +8,7 @@ import was.httpserver.HttpServlet;
 import was.httpserver.ServletManager;
 import was.httpserver.annotation.AnnotationServletV1;
 import was.httpserver.annotation.AnnotationServletV2;
+import was.httpserver.annotation.AnnotationServletV3;
 import was.v5.servlet.DiscardServlet;
 import was.v7.SearchControllerV7;
 import was.v7.SiteControllerV7;
@@ -18,7 +19,8 @@ public class ServerMainV8 {
 
 	public static void main(String[] args) throws IOException {
 		List<Object> controllers = List.of(new SiteControllerV8(), new SearchControllerV8());
-		HttpServlet servlet = new AnnotationServletV2(controllers);
+		// HttpServlet servlet = new AnnotationServletV2(controllers);
+		HttpServlet servlet = new AnnotationServletV3(controllers);
 
 		ServletManager servletManager = new ServletManager();
 		servletManager.setDefaultServlet(servlet);
