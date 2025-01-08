e5282c4311cb3a01d3d5800a3a4de4ab6d727d26
김지웅 <iop1996@easyshop.co.kr>
2025-01-08 17:51:20 +0900

    sleep을 사용해서 작업이 끝날 때까지 기다리기

---
+++ java-adv1/src/thread/control/join/JoinMainV2.java
@@ -0,0 +1,54 @@
+package thread.control.join;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+// V2는 Main 스레드를 멈춰버리면서 나머지 두개의 스레드가 작업을 마치도록 기다린다.
+public class JoinMainV2 {
+    public static void main(String[] args) {
+        log("start");
+        SumTask task1 = new SumTask(1, 50);
+        SumTask task2 = new SumTask(51, 100);
+        Thread thread1 = new Thread(task1, "thread-1");
+        Thread thread2 = new Thread(task2, "thread-2");
+
+        thread1.start();
+        thread2.start();
+
+        // 이 부분!
+        log("main 스레드 sleep()");
+        sleep(3000);
+        log("main 스레드 깨어남");
+
+        log("task1.result = " + task1.result);
+        log("task2.result = " + task2.result);
+
+        int sumAll = task1.result + task2.result;
+        log("task1 + task2 = " + sumAll);
+        log("end");
+    }
+
+    // SumTask는 계산의 시작값과 마지막 값을 가진다.
+    static class SumTask implements Runnable {
+        int startValue;
+        int endValue;
+        int result = 0;
+
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
+
+        @Override
+        public void run() {
+            log("작업 시작");
+            sleep(2000);
+            int sum = 0;
+            for (int i = startValue; i <= endValue; i++) {
+                sum += i;
+            }
+            result = sum;
+            log("작업 완료 result=" + result);
+        }
+    }
+}
