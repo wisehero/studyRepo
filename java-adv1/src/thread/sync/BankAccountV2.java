ad9597c429f451ce819c6a5a1d83dcc4432897e4
김지웅 <iop1996@easyshop.co.kr>
2025-01-10 16:02:31 +0900

    synchronized의 범위를 줄이기

--- java-adv1/src/thread/sync/BankAccountV2.java
+++ java-adv1/src/thread/sync/BankAccountV2.java
@@ -15,6 +15,8 @@ public class BankAccountV2 implements BankAccount {
     public synchronized boolean withdraw(int amount) {
         log("거래 시작: " + getClass().getSimpleName());
 
+
+        // 임계 영역 시작
         log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
         if (balance < amount) {
             log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
@@ -24,6 +26,7 @@ public class BankAccountV2 implements BankAccount {
         sleep(1000);
         balance = balance - amount;
         log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        // 임계 영역 종료
 
         log("거래 종료");
         return false;
---
+++ java-adv1/src/thread/sync/BankAccountV3.java
@@ -0,0 +1,38 @@
+package thread.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV3 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV3(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public  boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+
+        synchronized (this) {
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000);
+            balance = balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        }
+
+        log("거래 종료");
+        return false;
+    }
+
+    @Override
+    public int getBalance() {
+        return balance;
+    }
+}
