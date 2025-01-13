6d443559bff85a711a65d2a3d584b29764f2b5a5
김지웅 <iop1996@easyshop.co.kr>
2025-01-13 13:38:46 +0900

    tryLock() 을 이용해서 출금 예제를 구현 V2

---
+++ java-adv1/src/thread/sync/BankAccountV6.java
@@ -0,0 +1,60 @@
+package thread.sync;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV6 implements BankAccount {
+
+    private int balance;
+
+    private final Lock lock = new ReentrantLock(); // 락 객체 생성
+
+    public BankAccountV6(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+
+        try {
+            if (!lock.tryLock(500, TimeUnit.MILLISECONDS)) { // 락이 없을 때 대기할 시간을 지정
+                log("[진입 실패] 이미 처리중인 작업이 있습니다.");
+                return false; // 즉시 포기!
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000);
+            balance = balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        } finally {
+            lock.unlock(); // 락 해제
+        }
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
