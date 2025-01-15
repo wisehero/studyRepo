130d66cf1965d7e97fe2453da5e2570d37f4090e
김지웅 <iop1996@easyshop.co.kr>
2025-01-15 14:25:38 +0900

    생산자 소비자 문제 V4 - Condition 객체왜 signal(), await()

--- java-adv1/src/thread/bounded/BoundedMain.java
+++ java-adv1/src/thread/bounded/BoundedMain.java
@@ -10,8 +10,8 @@ public class BoundedMain {
     public static void main(String[] args) {
 //        BoundedQueue queue = new BoundedQueueV1(2);
 //        BoundedQueue queue = new BoundedQueueV2(2);
-        BoundedQueue queue = new BoundedQueueV3(2);
-
+//        BoundedQueue queue = new BoundedQueueV3(2);
+        BoundedQueue queue = new BoundedQueueV4(2);
 //        producerFirst(queue);
         consumerFirst(queue);
 
