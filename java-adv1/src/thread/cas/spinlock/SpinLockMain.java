7d4e1609155904fe99cd35983049ebf9d9c254df
김지웅 <iop1996@easyshop.co.kr>
2025-01-17 11:31:57 +0900

    CAS 락 구현 - 동시에 락을 얻는 실패 상황을 만들기

---
+++ java-adv1/src/thread/cas/spinlock/SpinLockMain.java
@@ -0,0 +1,24 @@
+package thread.cas.spinlock;
+
+import static util.MyLogger.log;
+
+public class SpinLockMain {
+    public static void main(String[] args) {
+        SpinLockBad spinLock = new SpinLockBad();
+
+        Runnable task = () -> {
+            spinLock.lock();
+            try {
+                log("비즈니스 로직 실행");
+            } finally {
+                spinLock.unlock();
+            }
+        };
+
+        Thread t1 = new Thread(task, "Thread-1");
+        Thread t2 = new Thread(task, "Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+}
