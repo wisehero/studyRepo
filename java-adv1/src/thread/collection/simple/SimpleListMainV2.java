c03f160433ca37555a612ea66f8ea999fb1e7ab9
김지웅 <iop1996@easyshop.co.kr>
2025-01-17 11:54:11 +0900

    동시성 컬렉션 - 컬렉션 프레임워크는 스레드 세이프에 안전하지 않은 것도 있다.

---
+++ java-adv1/src/thread/collection/simple/SimpleListMainV2.java
@@ -0,0 +1,35 @@
+package thread.collection.simple;
+
+import thread.collection.simple.list.BasicList;
+import thread.collection.simple.list.SimpleList;
+
+import static util.MyLogger.log;
+
+public class SimpleListMainV2 {
+    public static void main(String[] args) throws InterruptedException {
+        test(new BasicList());
+    }
+
+    private static void test(SimpleList list) throws InterruptedException {
+        log(list.getClass().getSimpleName());
+
+        Runnable addA = () -> {
+            list.add("A");
+            log("Thread-1: list.add(A)");
+        };
+
+        Runnable addB = () -> {
+            list.add("B");
+            log("Thread-2: list.add(B)");
+        };
+
+        Thread thread1 = new Thread(addA, "Thread-1");
+        Thread thread2 = new Thread(addB, "Thread-2");
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        log(list);
+    }
+}
