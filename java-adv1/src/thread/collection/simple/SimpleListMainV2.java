2baf24605af84f00fa7af2f2c8cf118f7f4f8992
김지웅 <iop1996@easyshop.co.kr>
2025-01-17 12:09:03 +0900

    동시성 컬렉션 - 스레드 세이프하게 모든 메서드에 synchronized 추가

--- java-adv1/src/thread/collection/simple/SimpleListMainV2.java
+++ java-adv1/src/thread/collection/simple/SimpleListMainV2.java
@@ -2,13 +2,16 @@ package thread.collection.simple;
 
 import thread.collection.simple.list.BasicList;
 import thread.collection.simple.list.SimpleList;
+import thread.collection.simple.list.SyncList;
 
 import static util.MyLogger.log;
 
 public class SimpleListMainV2 {
     public static void main(String[] args) throws InterruptedException {
-        test(new BasicList());
+//        test(new BasicList());
+        test(new SyncList());
     }
+
 
     private static void test(SimpleList list) throws InterruptedException {
         log(list.getClass().getSimpleName());
