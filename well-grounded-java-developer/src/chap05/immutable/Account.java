9157d986d17b430d3eefecbed89fe725eaa4def9
김지웅 <iop1996@easyshop.co.kr>
2025-02-06 15:41:49 +0900

    자바 동시성 기초 - 계좌 예제

--- well-grounded-java-developer/src/chap05/immutable/Account.java
+++ well-grounded-java-developer/src/chap05/immutable/Account.java
@@ -1,4 +1,55 @@
 package chap05.immutable;
 
 public class Account {
+    private double balance;
+
+    public Account(int openingBalance) {
+        this.balance = openingBalance;
+    }
+
+    public boolean rawWithdraw(int amount) {
+        if (balance >= amount) {
+            balance = balance - amount;
+            return true;
+        }
+        return false;
+    }
+
+    public void rawDeposit(int amount) {
+        balance = balance + amount;
+    }
+
+    public double getRawBalance() {
+        return balance;
+    }
+
+    public boolean safeWithdraw(final int amount) {
+        synchronized (this) {
+            if (balance >= amount) {
+                balance = balance - amount;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void safeDeposit(final int amount) {
+        synchronized (this) {
+            balance = balance + amount;
+        }
+    }
+
+    public double getSafeBalance() {
+        synchronized (this) {
+            return balance;
+        }
+    }
+
+    public boolean withdraw(int amount, boolean safe) {
+        if (safe) {
+            return safeWithdraw(amount);
+        } else {
+            return rawWithdraw(amount);
+        }
+    }
 }
