773ffa28b93c28b6a001e7d457839fb94b0bbcfb
wisehero <godjw96@gmail.com>
2025-02-24 00:10:33 +0900

    projec init

---
+++ settlementsystem/src/main/java/wisehero/settlementsystem/SettlementsystemApplication.java
@@ -0,0 +1,13 @@
+package wisehero.settlementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementsystemApplication.class, args);
+	}
+
+}
---
+++ settlementsystem/src/test/java/wisehero/settlementsystem/SettlementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package wisehero.settlementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
