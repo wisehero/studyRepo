b2c940b2ee28de3f96c7a4736f25093c07078e04
wisehero <godjw96@gmail.com>
2025-03-27 20:29:54 +0900

    [람다] - 추상메서드를 만들고 람다 사용하기

---
+++ java-adv3/src/lambda/lambda1/SamMain.java
@@ -0,0 +1,11 @@
+package lambda.lambda1;
+
+public class SamMain {
+
+	public static void main(String[] args) {
+		SamInterface samInterface = () -> {
+			System.out.println("sam");
+		};
+		samInterface.run();
+	}
+}
