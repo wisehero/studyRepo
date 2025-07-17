88c90500544e9b94736311c9d575196b65d41fa8
김지웅 <iop1996@easyshop.co.kr>
2025-07-17 14:47:29 +0900

    [Optional] - Optional 실전 예제

---
+++ java-adv3/src/optional/AddressMain2.java
@@ -0,0 +1,31 @@
+package optional;
+
+import optional.model.Address;
+import optional.model.User;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class AddressMain2 {
+
+    public static void main(String[] args) {
+        User user1 = new User("user1", null);
+        User user2 = new User("user2", new Address("hello street"));
+
+        printStreet(user1);
+        printStreet(user2);
+    }
+
+    static void printStreet(User user) {
+        getUserStreet(user).ifPresentOrElse(
+                System.out::println,
+                () -> System.out.println("Unknown")
+        );
+    }
+
+    static Optional<String> getUserStreet(User user) {
+        return Optional.ofNullable(user)
+                .map(User::getAddress)
+                .map(Address::getStreet);
+    }
+}
