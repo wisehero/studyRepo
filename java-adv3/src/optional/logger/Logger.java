14f1e725f7298500b14b70dccc1f44667136033a
김지웅 <iop1996@easyshop.co.kr>
2025-07-17 14:28:40 +0900

    [Optional] - 즉시평가와 지연평가

---
+++ java-adv3/src/optional/logger/Logger.java
@@ -0,0 +1,29 @@
+package optional.logger;
+
+import java.util.function.Supplier;
+
+public class Logger {
+
+    private boolean isDebug = false;
+
+    public boolean isDebug() {
+        return isDebug;
+    }
+
+    public void setDebug(boolean debug) {
+        isDebug = debug;
+    }
+
+    // DEBUG로 설정한 경우만 출력
+    public void debug(Object message) {
+        if(isDebug) {
+            System.out.println("[DEBUG] " + message);
+        }
+    }
+
+    public void debug(Supplier<?> supplier) {
+        if (isDebug) {
+            System.out.println("[DEBUG] " + supplier.get());
+        }
+    }
+}
