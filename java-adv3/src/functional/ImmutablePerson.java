4c552a4d3d409e8d57b51a3fa38a828d5d517550
김지웅 <godjw96@gmail.com>
2025-08-01 15:47:36 +0900

    [함수형 프로그래밍] 불변객체 활용

---
+++ java-adv3/src/functional/ImmutablePerson.java
@@ -0,0 +1,32 @@
+package functional;
+
+public class ImmutablePerson {
+
+    private final String name;
+    private final int age;
+
+    public ImmutablePerson(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public ImmutablePerson withAge(int newAge) {
+        return new ImmutablePerson(name, newAge);
+    }
+
+    @Override
+    public String toString() {
+        return "ImmutablePerson{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ java-adv3/src/functional/MutablePerson.java
@@ -0,0 +1,37 @@
+package functional;
+
+import java.util.StringJoiner;
+
+public class MutablePerson {
+    private String name;
+    private int age;
+
+    public MutablePerson(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", MutablePerson.class.getSimpleName() + "[", "]")
+                .add("name='" + name + "'")
+                .add("age=" + age)
+                .toString();
+    }
+}
